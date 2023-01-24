7c50de9d8a3f86892827191408b10bc1f049e60c
josea <jose.a.olmosmartinez@gmail.com>
2023-01-24 21:52:42 +0100

    Se añade y configura app portfolio

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Project
 # Register your models here.
+
+class ProjectAdmin(admin.ModelAdmin):   # Configuracion extendida con campos de solo lectura cread y updated
+    readonly_fields = ('created', 'updated')
+
+admin.site.register(Project, ProjectAdmin)
+
+
